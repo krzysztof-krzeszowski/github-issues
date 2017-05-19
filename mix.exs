abb5f2cad30859b468d9d448c0c10fb14267d47f
Krzysztof Krzeszowski <krzysztof.krzeszowski@gmail.com>
2017-05-19 09:22:14 +0200

    coveralls settings

--- mix.exs
+++ mix.exs
@@ -10,6 +10,13 @@ defmodule Issues.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: [
+      "coveralls": :test,
+      "coveralls.detail": :test,
+      "coveralls.post": :test,
+      "coveralls.html": :test
+     ],
      deps: deps()]
   end
 
